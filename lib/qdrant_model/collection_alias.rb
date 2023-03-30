e541fee2a4168569455d1b986670e5d85f101ede
Andersen Fan <as181920@gmail.com>
2023-03-30 16:14:22 +0800

    Feat: implement CollectionAlias

---
+++ lib/qdrant_model/collection_alias.rb
@@ -0,0 +1,54 @@
+module QdrantModel
+  class CollectionAlias < ApiClient
+    include ActiveModel::API
+
+    attr_accessor :collection_name, :alias_name
+
+    class << self
+      def create(collection_name:, alias_name:)
+        post(
+          "/collections/aliases",
+          { actions: { create_alias: { collection_name:, alias_name: } } }.to_json
+        ).then do |resp_info|
+          resp_info["status"] == "ok" ? new(collection_name:, alias_name:) : new(collection_name:, alias_name: nil)
+        end
+      end
+
+      def update(old_alias_name:, new_alias_name:)
+        post(
+          "/collections/aliases",
+          { actions: { rename_alias: { old_alias_name:, new_alias_name: } } }.to_json
+        ).then do |resp_info|
+          resp_info["status"] == "ok" ? new(collection_name: nil, alias_name: new_alias_name) : new(collection_name: nil, alias_name: old_alias_name)
+        end
+      end
+
+      def destroy(alias_name)
+        post(
+          "/collections/aliases",
+          { actions: { delete_alias: { alias_name: } } }.to_json
+        ).then do |resp_info|
+          resp_info["status"] == "ok" ? new(collection_name: nil, alias_name: nil) : new(collection_name: nil, alias_name:)
+        end
+      end
+    end
+
+    def update(new_alias_name:)
+      post(
+        "/collections/aliases",
+        { actions: { rename_alias: { old_alias_name: alias_name, new_alias_name: } } }.to_json
+      ).then { |resp_info| resp_info["status"] == "ok" }
+        .tap { |result| result ? self.alias_name = new_alias_name : nil }
+    end
+
+    def destroy
+      post(
+        "/collections/aliases",
+        { actions: { delete_alias: { alias_name: } } }.to_json
+      ).then do |resp_info|
+        self.alias_name = nil if resp_info["status"] == "ok"
+        self
+      end
+    end
+  end
+end
---
+++ test/qdrant_model/collection_alias_test.rb
@@ -0,0 +1,36 @@
+require "test_helper"
+
+module QdrantModel
+  describe CollectionAlias do
+    before do
+      @collection = Collection.new name: "old_name"
+
+      stub_request(:post, "http://127.0.0.1:6333/collections/aliases").to_return(
+        status: 200,
+        body: { time: 0, status: "ok", result: true }.to_json
+      )
+    end
+
+    it "should create alias name for collection" do
+      collection_alias = CollectionAlias.create collection_name: @collection.name, alias_name: "alias_name"
+
+      assert_equal "alias_name", collection_alias.alias_name
+    end
+
+    it "should update alias name" do
+      collection_alias = CollectionAlias.new(collection_name: "c_name", alias_name: "a_name")
+
+      assert_equal "a_name_2", CollectionAlias.update(old_alias_name: "a_name", new_alias_name: "a_name_2").alias_name
+      assert collection_alias.update(new_alias_name: "a_name_3")
+      assert_equal "a_name_3", collection_alias.alias_name
+    end
+
+    it "should destroy alias name" do
+      collection_alias = CollectionAlias.new(collection_name: "c_name", alias_name: "a_name")
+
+      assert_nil CollectionAlias.destroy(alias_name: collection_alias.alias_name).alias_name
+      assert collection_alias.destroy
+      assert_nil collection_alias.alias_name
+    end
+  end
+end
